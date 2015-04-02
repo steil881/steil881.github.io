92647ed28016188a406e8205ff2df537a7eef641
steil881 <steil881@newschool.edu>
2015-04-02 13:28:24 +0000

    Adding index file

---
+++ lab/loop/loop.js
@@ -0,0 +1,272 @@
+var lines = ["*                    *", " *                  *", "  *                *", "   *              *", "    *            *", "     *          *", "      *        *", "       *      *", "        *    *", "         *  *", "          **",]
+
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
+for (var index=0; index <=lines.length; index++){
+	console.log (lines [index]);
+}
+for (var index =lines.length; index >=0; index--){
+	console.log (lines [index]);
+}
